c54fa31967c91a5eb5c5c1060f2b0de6a6e6cbfb
Fernando Paredes García <fernando@develcuy.com>
2012-05-31 22:04:35 -0500

    Credits and Coding Standards.

--- cgic.h
+++ cgic.h
@@ -1,24 +1,33 @@
-/* The CGI_C library, by Thomas Boutell, version 2.01. CGI_C is intended
-	to be a high-quality API to simplify CGI programming tasks. */
+/**
+ * The CGIC-Lua extension by Fernando Paredes García.
+ *
+ * CGIC-Lua is intended to be a high-quality API to simplify CGI
+ * programming tasks.
+ *
+ * Based on CGIC_C library - (c) Thomas Boutell and Boutell.Com, Inc. 1996-2011
+ * Version 2.05
+ * Site:   https://www.boutell.com/
+ */
 
-/* Make sure this is only included once. */
-
+// Make sure this is only included once.
 #ifndef CGI_C
 #define CGI_C 1
 
-/* Bring in standard I/O since some of the functions refer to
-	types defined by it, such as FILE *. */
-
+/**
+ * Bring in standard I/O since some of the functions refer to
+ * types defined by it, such as FILE *.
+ */
 #include <stdio.h>
 
-/* The various CGI environment variables. Instead of using getenv(),
-	the programmer should refer to these, which are always
-	valid null-terminated strings (they may be empty, but they 
-	will never be null). If these variables are used instead
-	of calling getenv(), then it will be possible to save
-	and restore CGI environments, which is highly convenient
-	for debugging. */
-
+/** 
+ * The various CGI environment variables. Instead of using getenv(),
+ *	the programmer should refer to these, which are always
+ * valid null-terminated strings (they may be empty, but they 
+ * will never be null). If these variables are used instead
+ * of calling getenv(), then it will be possible to save
+ * and restore CGI environments, which is highly convenient
+ * for debugging.
+*/
 extern char *cgiServerSoftware;
 extern char *cgiServerName;
 extern char *cgiGatewayInterface;
@@ -39,37 +48,46 @@ extern char *cgiAccept;
 extern char *cgiUserAgent;
 extern char *cgiReferrer;
 
-/* Cookies as sent to the server. You can also get them
-	individually, or as a string array; see the documentation. */
+/**
+ * Cookies as sent to the server. You can also get them
+ * individually, or as a string array; see the documentation.
+ */
 extern char *cgiCookie;
 
-/* A macro providing the same incorrect spelling that is
-	found in the HTTP/CGI specifications */
+/**
+ * A macro providing the same incorrect spelling that is
+ * found in the HTTP/CGI specifications
+ */
 #define cgiReferer cgiReferrer
 
-/* The number of bytes of data received.
-	Note that if the submission is a form submission
-	the library will read and parse all the information
-	directly from cgiIn; the programmer need not do so. */
-
+/**
+ * The number of bytes of data received.
+ * Note that if the submission is a form submission
+ * the library will read and parse all the information
+ * directly from cgiIn; the programmer need not do so.
+ */
 extern int cgiContentLength;
 
-/* Pointer to CGI output. The cgiHeader functions should be used
-	first to output the mime headers; the output HTML
-	page, GIF image or other web document should then be written
-	to cgiOut by the programmer. In the standard CGIC library,
-	cgiOut is always equivalent to stdout. */
-
+/**
+ * Pointer to CGI output. The cgiHeader functions should be used
+ * first to output the mime headers; the output HTML
+ * page, GIF image or other web document should then be written
+ * to cgiOut by the programmer. In the standard CGIC library,
+ * cgiOut is always equivalent to stdout.
+ */
 extern FILE *cgiOut;
 
-/* Pointer to CGI input. The programmer does not read from this.
-	We have continued to export it for backwards compatibility
-	so that cgic 1.x applications link properly. */
-
+/**
+ * Pointer to CGI input. The programmer does not read from this.
+ * We have continued to export it for backwards compatibility
+ * so that cgic 1.x applications link properly.
+ */
 extern FILE *cgiIn;
 
-/* Possible return codes from the cgiForm family of functions (see below). */
-
+/**
+ * Possible return codes from the cgiForm family of functions
+ * (see below).
+ */
 typedef enum {
 	cgiFormSuccess,
 	cgiFormTruncated,
@@ -87,48 +105,58 @@ typedef enum {
 	cgiFormEOF
 } cgiFormResultType;
 
-/* These functions are used to retrieve form data. See
-	cgic.html for documentation. */
-
+/**
+ * These functions are used to retrieve form data. See
+ * cgic.html for documentation.
+ */
 extern cgiFormResultType cgiFormString(
-	char *name, char *result, int max);
+	char *name, char *result, int max
+);
 
 extern cgiFormResultType cgiFormStringNoNewlines(
-	char *name, char *result, int max);
-
+	char *name, char *result, int max
+);
 
 extern cgiFormResultType cgiFormStringSpaceNeeded(
-	char *name, int *length);
-
+	char *name, int *length
+);
 
 extern cgiFormResultType cgiFormStringMultiple(
-	char *name, char ***ptrToStringArray);
+	char *name, char ***ptrToStringArray
+);
 
 extern void cgiStringArrayFree(char **stringArray);
 
 extern cgiFormResultType cgiFormInteger(
-	char *name, int *result, int defaultV);
+	char *name, int *result, int defaultV
+);
 
 extern cgiFormResultType cgiFormIntegerBounded(
-	char *name, int *result, int min, int max, int defaultV);
+	char *name, int *result, int min, int max, int defaultV
+);
 
 extern cgiFormResultType cgiFormDouble(
-	char *name, double *result, double defaultV);
+	char *name, double *result, double defaultV
+);
 
 extern cgiFormResultType cgiFormDoubleBounded(
-	char *name, double *result, double min, double max, double defaultV);
+	char *name, double *result, double min, double max, double defaultV
+);
 
 extern cgiFormResultType cgiFormSelectSingle(
 	char *name, char **choicesText, int choicesTotal, 
-	int *result, int defaultV);	
+	int *result, int defaultV
+);	
 
 
 extern cgiFormResultType cgiFormSelectMultiple(
 	char *name, char **choicesText, int choicesTotal, 
-	int *result, int *invalid);
+	int *result, int *invalid
+);
 
 extern cgiFormResultType cgiFormCheckboxSingle(
-	char *name);
+	char *name
+);
 
 extern cgiFormResultType cgiFormCheckboxMultiple(
 	char *name, char **valuesText, int valuesTotal, 
@@ -136,50 +164,63 @@ extern cgiFormResultType cgiFormCheckboxMultiple(
 
 extern cgiFormResultType cgiFormRadio(
 	char *name, char **valuesText, int valuesTotal, 
-	int *result, int defaultV);	
+	int *result, int defaultV
+);	
 
-/* The paths returned by this function are the original names of files
-	as reported by the uploading web browser and shoult NOT be
-	blindly assumed to be "safe" names for server-side use! */
+/**
+ * The paths returned by this function are the original names of files
+ * as reported by the uploading web browser and shoult NOT be
+ * blindly assumed to be "safe" names for server-side use!
+ */
 extern cgiFormResultType cgiFormFileName(
-	char *name, char *result, int max);
+	char *name, char *result, int max
+);
 
-/* The content type of the uploaded file, as reported by the browser.
-	It should NOT be assumed that browsers will never falsify
-	such information. */
+/** The content type of the uploaded file, as reported by the browser.
+ * It should NOT be assumed that browsers will never falsify
+ * such information.
+ */
 extern cgiFormResultType cgiFormFileContentType(
-	char *name, char *result, int max);
+	char *name, char *result, int max
+);
 
 extern cgiFormResultType cgiFormFileSize(
-	char *name, int *sizeP);
+	char *name, int *sizeP
+);
 
 typedef struct cgiFileStruct *cgiFilePtr;
 
 extern cgiFormResultType cgiFormFileOpen(
-	char *name, cgiFilePtr *cfpp);
+	char *name, cgiFilePtr *cfpp
+);
 
 extern cgiFormResultType cgiFormFileRead(
-	cgiFilePtr cfp, char *buffer, int bufferSize, int *gotP);
+	cgiFilePtr cfp, char *buffer, int bufferSize, int *gotP
+);
 
-extern cgiFormResultType cgiFormFileClose(
-	cgiFilePtr cfp);
+extern cgiFormResultType cgiFormFileClose(cgiFilePtr cfp);
 
 extern cgiFormResultType cgiCookieString(
-	char *name, char *result, int max);
+	char *name, char *result, int max
+);
 
 extern cgiFormResultType cgiCookieInteger(
-	char *name, int *result, int defaultV);
+	char *name, int *result, int defaultV
+);
 
-cgiFormResultType cgiCookies(
-	char ***ptrToStringArray);
+cgiFormResultType cgiCookies(char ***ptrToStringArray);
 
-/* path can be null or empty in which case a path of / (entire site) is set. 
-	domain can be a single web site; if it is an entire domain, such as
-	'boutell.com', it should begin with a dot: '.boutell.com' */
+/**
+ * path can be null or empty in which case a path of / (entire site) is set. 
+ * domain can be a single web site; if it is an entire domain, such as
+ * 'boutell.com', it should begin with a dot: '.boutell.com'
+ */
 extern void cgiHeaderCookieSetString(char *name, char *value, 
-	int secondsToLive, char *path, char *domain);
+	int secondsToLive, char *path, char *domain
+);
 extern void cgiHeaderCookieSetInteger(char *name, int value,
-	int secondsToLive, char *path, char *domain);
+	int secondsToLive, char *path, char *domain
+);
 extern void cgiHeaderLocation(char *redirectUrl);
 extern void cgiHeaderStatus(int status, char *statusMessage);
 extern void cgiHeaderContentType(char *mimeType);
@@ -194,34 +235,41 @@ typedef enum {
 extern cgiEnvironmentResultType cgiWriteEnvironment(char *filename);
 extern cgiEnvironmentResultType cgiReadEnvironment(char *filename);
 
-extern cgiFormResultType cgiFormEntries(
-	char ***ptrToStringArray);
+extern cgiFormResultType cgiFormEntries(char ***ptrToStringArray);
 
-/* Output string with the <, &, and > characters HTML-escaped. 
-	's' is null-terminated. Returns cgiFormIO in the event
-	of error, cgiFormSuccess otherwise. */
+/**
+ * Output string with the <, &, and > characters HTML-escaped. 
+ * 's' is null-terminated. Returns cgiFormIO in the event
+ * of error, cgiFormSuccess otherwise.
+ */
 cgiFormResultType cgiHtmlEscape(char *s);
 
-/* Output data with the <, &, and > characters HTML-escaped. 
-	'data' is not null-terminated; 'len' is the number of
-	bytes in 'data'. Returns cgiFormIO in the event
-	of error, cgiFormSuccess otherwise. */
+/**
+ * Output data with the <, &, and > characters HTML-escaped. 
+ * 'data' is not null-terminated; 'len' is the number of
+ * bytes in 'data'. Returns cgiFormIO in the event
+ * of error, cgiFormSuccess otherwise.
+ */
 cgiFormResultType cgiHtmlEscapeData(char *data, int len);
 
-/* Output string with the " character HTML-escaped, and no
-	other characters escaped. This is useful when outputting
-	the contents of a tag attribute such as 'href' or 'src'.
-	's' is null-terminated. Returns cgiFormIO in the event
-	of error, cgiFormSuccess otherwise. */
+/**
+ * Output string with the " character HTML-escaped, and no
+ * other characters escaped. This is useful when outputting
+ * the contents of a tag attribute such as 'href' or 'src'.
+ * 's' is null-terminated. Returns cgiFormIO in the event
+ * of error, cgiFormSuccess otherwise.
+ */
 cgiFormResultType cgiValueEscape(char *s);
 
-/* Output data with the " character HTML-escaped, and no
-	other characters escaped. This is useful when outputting
-	the contents of a tag attribute such as 'href' or 'src'.
-	'data' is not null-terminated; 'len' is the number of
-	bytes in 'data'. Returns cgiFormIO in the event
-	of error, cgiFormSuccess otherwise. */
+/**
+ * Output data with the " character HTML-escaped, and no
+ * other characters escaped. This is useful when outputting
+ * the contents of a tag attribute such as 'href' or 'src'.
+ * 'data' is not null-terminated; 'len' is the number of
+ * bytes in 'data'. Returns cgiFormIO in the event
+ * of error, cgiFormSuccess otherwise.
+ */
 cgiFormResultType cgiValueEscapeData(char *data, int len);
 
-#endif /* CGI_C */
+#endif // CGI_C
 
